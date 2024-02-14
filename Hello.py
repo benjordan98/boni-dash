96a05330d914a4f0d9b5953bd1379d5923c9de39
benjordan98 <94760497+benjordan98@users.noreply.github.com>
2024-02-14 13:42:03 +0000

    fixing deployment error

--- Hello.py
+++ Hello.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 import altair as alt
 from streamlit.logger import get_logger
