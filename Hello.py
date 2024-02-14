e0b6b79a72d6d779f6e6935dea27dac1b9925ca3
benjordan98 <94760497+benjordan98@users.noreply.github.com>
2024-02-14 10:47:12 +0000

    got ride of old template stuff

--- Hello.py
+++ Hello.py
@@ -8,14 +8,6 @@ from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
 
-def empty_placeholder_plot(ax):
-    ax.set_axis_off()  # Hide axes
-    ax.text(0.5, 0.5, "Placeholder Plot", ha="center", va="center", fontsize=12, color="gray")
-
-def calculate_boni_performance(df):
-    # TODO: Implement function to calculate 
-    return 0.75
-
 def boni_spend_per_month(df):
     # group data by month
     by_month = df.groupby(df['date'].dt.to_period('M'))['discount_meal_price'].sum()
@@ -47,8 +39,8 @@ def pre_process_df(df):
 
 def run():
     st.set_page_config(
-        page_title="dashboard",
-        page_icon="👋",
+        page_title="Dashboard",
+        page_icon="🍕"
     )
     img, heading = st.columns([1,8])
     image_path = "boni-removebg-preview.png"  # Replace with the actual path to your image file
@@ -67,57 +59,27 @@ def run():
 
     # Column 1 row 1
     restaurant_counts = df['restaurant'].value_counts()
-    # add column for whether there was 1 visit, 1 - 2 visits, or more than 2 visits
     restaurant_counts = pd.DataFrame(restaurant_counts)
-    restaurant_counts.columns = ['count']
-    #TODO: coloring and sorting
-    restaurant_counts
+    restaurant_counts = restaurant_counts.reset_index()
+    restaurant_counts.columns = ['Restaurant', 'Count']
     chart0 = alt.Chart(restaurant_counts.reset_index()).mark_bar().encode(
-        x = "restaurants",
-        y = "count"
-        # color = "skyblue"
+        x = alt.X('Restaurant:N', sort="-y", axis=alt.Axis(title='Restaurant', labels=True, ticks=True)),
+        y = "Count"
+    ).configure_mark(
+        color = "#23BDF3"
     )
     col11.altair_chart(chart0)
     # col11.pyplot(fig1, clear_figure=True)
 
     # Column 2 row 1
-    fig2 = plt.figure(figsize=(4, 4.21))
-    # colors and values
-    colors = ['#4dab6d', "#72c66e", "#c1da64", "#f6ee54", "#fabd57", "#f36d54", "#ee4d55"]
-    values = [100,80,60,40,20,0,-20, -40]
-    x_axis_vals = [0, 0.44, 0.88,1.32,1.76,2.2,2.64]
-
-    ax2 = fig2.add_subplot(projection="polar");
-
-    ax2.bar(x=[0, 0.44, 0.88,1.32,1.76,2.2,2.64], width=0.5, height=0.5, bottom=2,
-        linewidth=3, edgecolor="white",
-        color=colors, align="edge");
-
-    plt.annotate("High Performing", xy=(0.16,2.1), rotation=-75, color="white", fontweight="bold");
-    plt.annotate("Sustainable", xy=(0.65,2.08), rotation=-55, color="white", fontweight="bold");
-    plt.annotate("Maturing", xy=(1.14,2.1), rotation=-32, color="white", fontweight="bold");
-    plt.annotate("Developing", xy=(1.62,2.2), color="white", fontweight="bold");
-    plt.annotate("Foundational", xy=(2.08,2.25), rotation=20, color="white", fontweight="bold");
-    plt.annotate("Volatile", xy=(2.46,2.25), rotation=45, color="white", fontweight="bold");
-    plt.annotate("Unsustainable", xy=(3.0,2.25), rotation=75, color="white", fontweight="bold");
-
-    for loc, val in zip([0, 0.44, 0.88,1.32,1.76,2.2,2.64, 3.14], values):
-        plt.annotate(val, xy=(loc, 2.5), ha="right" if val<=20 else "left");
-
-    plt.annotate("50", xytext=(0,0), xy=(1.1, 2.0),
-                arrowprops=dict(arrowstyle="wedge, tail_width=0.5", color="black", shrinkA=0),
-                bbox=dict(boxstyle="circle", facecolor="black", linewidth=2.0, ),
-                fontsize=45, color="white", ha="center"
-                );
-
-
-    plt.title("Performance Gauge Chart", loc="center", pad=20, fontsize=35, fontweight="bold");
-
-    ax2.set_axis_off()
-    col12.pyplot(fig2, clear_figure=True)
+    # col12.subheader("Summary")
+    # col12.write("Top Boni: Zito")
+    # col12.write("Total Boni Trips: 57")
+    # col12.write("Unique Boni Tried: 25")
+    # col12.write("Avg Meal Price: 2.5 EURO")
 
     # Second row of plots
-    col21, col22, col23 = st.columns([1, 1, 1])
+    col21, col22= st.columns([1, 1])
 
     # row 2 column 1
     df_by_month = boni_spend_per_month(df)
@@ -138,12 +100,9 @@ def run():
         tooltip=['month_name:N', 'cost:Q', 'Budget Status:N']
     ).properties(
         title='Boni cost by month'
-    ).configure_axis(
-        labelAngle=45
     )
     col21.altair_chart(chart)
 
-    fig4, ax4 = plt.subplots()
     boni_usage_by_month = boni_usage_per_month(df)
     month_order = ['October', 'November', 'December', 'January', 'February']
     # altair chart
@@ -151,15 +110,11 @@ def run():
         x = alt.X('month_name:N', sort=month_order, axis=alt.Axis(title='Month', labels=True, ticks=True)),
         y = "value",
         color = alt.Color('type:N', scale=alt.Scale(domain=['used', 'unused'], range=['skyblue', 'lightcoral']))
+    ).properties(
+        title = "Boni utilisation by month"
     )
 
     col22.altair_chart(chart2)
-
-    col23.subheader("Summary")
-    col23.write("Top Boni: Zito")
-    col23.write("Total Boni Trips: 57")
-    col23.write("Unique Boni Tried: 25")
-    col23.write("Avg Meal Price: 2.5 EURO")
 
 if __name__ == "__main__":
     run()
