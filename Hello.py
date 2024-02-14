1bbda6cf12e7496b1a5ca9995f1eeb5f22a8be40
benjordan98 <94760497+benjordan98@users.noreply.github.com>
2024-02-14 11:34:48 +0000

    pretty good for now

--- Hello.py
+++ Hello.py
@@ -19,28 +19,46 @@ def boni_usage_per_month(df):
     # group data by month
     by_month = df.groupby(df['date'].dt.to_period('M')).agg('count')['date']
     df_by_month = pd.DataFrame(by_month)
-    df_by_month.columns = ['used']
+    df_by_month.columns = ['Used']
     # number of working days in each month
     working_days = [21, 21, 19, 21, 20]
     df_by_month['working_days'] = working_days
     # un-used boni
-    df_by_month['unused'] = df_by_month['working_days'] - df_by_month['used']
+    df_by_month['Unused'] = df_by_month['working_days'] - df_by_month['Used']
     df_by_month.drop('working_days', axis = 1, inplace = True)
     df_by_month['date'] = pd.to_datetime(df_by_month.index.astype(str))
     # Extract month names
     df_by_month['month_name'] = df_by_month['date'].dt.strftime('%B')
-    df.drop('date', axis = 1, inplace = True)
-    df_by_month = pd.melt(df_by_month, id_vars=['month_name'], var_name='type')
+    df_by_month.drop('date', axis = 1, inplace = True)
+    df_by_month = pd.melt(df_by_month, id_vars=['month_name'], var_name='Utilisation')
     return df_by_month
 
 def pre_process_df(df):
     # datetime
     df['date'] = pd.to_datetime(df['date'])
 
+def get_top_boni(df):
+    # get the name of the restaurant with the most visits
+    top_restaurant = df['restaurant'].value_counts().idxmax()
+    return top_restaurant
+
+def get_longest_streak(df):
+    # identify the longest streak of consecutive days with a visit to any restaurant
+    df['date'] = pd.to_datetime(df['date'])
+    df['date_diff'] = df['date'].diff().dt.days
+    df['date_diff'] = df['date_diff'].fillna(1)
+    print(df)
+    df['date_diff'] = df['date_diff'].astype(int)
+    df['streak'] = (df['date_diff'] != 1).cumsum()
+    streaks = df.groupby('streak').size()
+    longest_streak = streaks.max()
+    return longest_streak
+
 def run():
     st.set_page_config(
         page_title="Dashboard",
-        page_icon="🍕"
+        page_icon="🍕",
+        layout="wide"
     )
     img, heading = st.columns([1,8])
     image_path = "boni-removebg-preview.png"  # Replace with the actual path to your image file
@@ -67,16 +85,24 @@ def run():
         y = "Count"
     ).configure_mark(
         color = "#23BDF3"
+    ).properties(
+        title = "Restaurants visited"
     )
-    col11.altair_chart(chart0)
+    col11.altair_chart(chart0, use_container_width=True)
     # col11.pyplot(fig1, clear_figure=True)
 
     # Column 2 row 1
-    # col12.subheader("Summary")
-    # col12.write("Top Boni: Zito")
-    # col12.write("Total Boni Trips: 57")
-    # col12.write("Unique Boni Tried: 25")
-    # col12.write("Avg Meal Price: 2.5 EURO")
+    col12.subheader("Summary")
+    top_boni = get_top_boni(df)
+    col12.text('🏆 Top Boni: ' + top_boni)
+    total_trips = df.shape[0]
+    col12.text("🔢 Total Boni Trips: " + str(total_trips))
+    unique_boni = len(df['restaurant'].unique())
+    col12.text("🆕 Unique Boni Tried: " + str(unique_boni))
+    avg_price = round(df['discount_meal_price'].mean(), 2)
+    col12.text("💶 Avg Meal Price: " + str(avg_price) + " EURO")
+    longest_streak = get_longest_streak(df)
+    col12.text("🗓️Longest Streak: " + str(longest_streak) + " Days")
 
     # Second row of plots
     col21, col22= st.columns([1, 1])
@@ -94,27 +120,32 @@ def run():
 
     # Altair chart
     chart = alt.Chart(df_by_month).mark_bar().encode(
-        x=alt.X('month_name:N', sort=month_order, axis=alt.Axis(title='Month', labels=True, ticks=True)),
-        y=alt.Y('cost:Q', axis=alt.Axis(title='Cost (EURO)')),
+        y=alt.Y('month_name:N', sort=month_order, axis=alt.Axis(title='Month', labels=True, ticks=True)),
+        x=alt.X('cost:Q', axis=alt.Axis(title='Cost (EURO)')),
         color=alt.Color('Budget Status:N', scale=alt.Scale(domain=['Under-budget', 'Over-budget'], range=['skyblue', 'lightcoral'])),
         tooltip=['month_name:N', 'cost:Q', 'Budget Status:N']
     ).properties(
-        title='Boni cost by month'
+        title='Boni Cost by Month'
     )
-    col21.altair_chart(chart)
+    col21.altair_chart(chart, use_container_width=True)
 
     boni_usage_by_month = boni_usage_per_month(df)
     month_order = ['October', 'November', 'December', 'January', 'February']
     # altair chart
     chart2 = alt.Chart(boni_usage_by_month).mark_bar().encode(
-        x = alt.X('month_name:N', sort=month_order, axis=alt.Axis(title='Month', labels=True, ticks=True)),
-        y = "value",
-        color = alt.Color('type:N', scale=alt.Scale(domain=['used', 'unused'], range=['skyblue', 'lightcoral']))
+        y = alt.Y('month_name:N', sort=month_order, axis=alt.Axis(title='Month', labels=True, ticks=True)),
+        x = "value",
+        color = alt.Color('Utilisation:N', scale=alt.Scale(domain=['Unused', 'Used'], range=['lightcoral', 'skyblue'])),
+        order=alt.Order(
+        # Sort the segments of the bars by this field
+        'Utilisation',
+        sort='descending'
+        )
     ).properties(
-        title = "Boni utilisation by month"
+        title = "Boni Utilisation by Month"
     )
 
-    col22.altair_chart(chart2)
+    col22.altair_chart(chart2, use_container_width=True)
 
 if __name__ == "__main__":
     run()
